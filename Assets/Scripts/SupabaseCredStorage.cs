5112db31052a77e6c2b9ce33ea16ce453dd59da9
Conwell Maraya <conwell.maraya@gmail.com>
2025-05-23 22:51:22 +0800

    Added storage for supabase url and api key
    Added a class that stores the supabase url and api key, for future contributors. Added a message to the file that informs said contributors of it's purpose

---
+++ Assets/Scripts/SupabaseCredStorage.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SupabaseCredStorage : MonoBehaviour
+{
+    //DO NOT TRACK THIS FILE IN YOUR REPO, CONTAINS THE API KEY AND THE SUPABASE URL FOR THE PROJECT
+
+    public string supabaseURL;
+    public string supabaseKey;
+}
