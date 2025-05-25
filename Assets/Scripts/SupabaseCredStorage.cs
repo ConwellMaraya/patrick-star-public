04b37ea66671953e105a9df99ed4cea787a87546
bencruto60 <68362559+bencruto60@users.noreply.github.com>
2025-05-25 09:15:12 +0800

    SupabaseCredStorage Reimplementation
    added supabasecred storage

---
+++ Assets/Scripts/SupabaseCredStorage.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SupabaseCredStorage : MonoBehaviour
+{
+    public string supabaseURL = "https://ziluhantkwazbkwbypzv.supabase.co";
+    public string supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InppbHVoYW50a3dhemJrd2J5cHp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc2NDA0NjMsImV4cCI6MjA2MzIxNjQ2M30.-6JEJ8nraBFnAGQRTsqa7fLjkgedTJs_IBloRnsKsPw";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
